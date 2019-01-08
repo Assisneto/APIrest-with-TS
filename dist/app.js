7683a6c8478d7af145ffe733b7995fd2738efb1a
Assisneto <netodamasceno12@gmail.com>
2019-01-08 17:10:22 -0300

    API complete

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
 const Routes_1 = require("./routes/Routes");
+const mongoose = require("mongoose");
 class App {
     constructor() {
         this.routes = new Routes_1.Routes();
+        this.mongoUrl = 'mongodb://assisneto:assisneto@cluster0-shard-00-00-qhyrk.mongodb.net:27017,cluster0-shard-00-01-qhyrk.mongodb.net:27017,cluster0-shard-00-02-qhyrk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
         this.app = express();
         this.config();
         this.routes.routes(this.app);
+        this.mongoSetup();
+    }
+    mongoSetup() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
     config() {
         // support application/json type post data
--- lib/app.ts
+++ lib/app.ts
@@ -7,14 +7,18 @@ class App {
 
     public app: express.Application;
     public routes: Routes =  new Routes();
-    
+    public mongoUrl: string = 'mongodb://assisneto:assisneto@cluster0-shard-00-00-qhyrk.mongodb.net:27017,cluster0-shard-00-01-qhyrk.mongodb.net:27017,cluster0-shard-00-02-qhyrk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 
     constructor() {
         this.app = express();
         this.config();        
         this.routes.routes(this.app);
+        this.mongoSetup();
     }
-
+    private mongoSetup(){
+      mongoose.Promise = global.Promise;
+      mongoose.connect(this.mongoUrl);
+    }
     private config(): void{
         // support application/json type post data
         this.app.use(bodyParser.json());
